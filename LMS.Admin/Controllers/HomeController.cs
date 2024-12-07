a6b280aff43e375c7c514e36f5159b2136406190
mayankrajdsm <mayankraj.dsm@gmail.com>
2024-12-07 20:07:32 +0530

    merge header and menu

--- LMS.Admin/Controllers/HomeController.cs
+++ LMS.Admin/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace LMS.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
